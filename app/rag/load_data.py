e3f6b9304304121911da98da2ae24d6e4620733a
kindredzhang <kindred.zhang.life@gmail.com>
2025-03-11 17:27:52 +0800

    vector data storing

--- app/rag/load_data.py
+++ app/rag/load_data.py
@@ -1,3 +1,4 @@
+import chromadb
 from llama_index.core import SimpleDirectoryReader
 from llama_index.core import VectorStoreIndex
 from llama_index.core import Document
@@ -7,16 +8,23 @@ from llama_index.core.schema import TextNode
 from llama_index.embeddings.openai import OpenAIEmbedding
 from llama_index.core.settings import Settings
 from llama_index.llms.openai import OpenAI
+from llama_index.core import StorageContext
+from llama_index.vector_stores.chroma import ChromaVectorStore
 import os
 
 def get_project_root() -> str:
     return os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 async def main():
+    # vector store
+    chroma = chromadb.PersistentClient(path="./chroma_db")
+
     # manual LLM
     llm = OpenAI(model = "gpt-4o")
+
     # manual text splitter
     text_splitter = SentenceSplitter(chunk_size=100, chunk_overlap=20)
+    
     # settings~
     Settings.llm = llm
     Settings.transformations = [text_splitter]
@@ -35,11 +43,18 @@ async def main():
     # document2 = Document(text="Hello llamaindex!", metadata={"source": "manual", "filename": "manual.txt"})
     # documents = [document1, document2]
 
+    # create chroma collection
+    chroma_collection = chroma.get_or_create_collection(name="kindred-zhang-chroma")
+    # create chroma vector store instance
+    chroma_vector_store = ChromaVectorStore(chroma_client=chroma, collection=chroma_collection)
+    # create storage context used for 'from_documents' function. as param of 'from_documents' can auto save vector data to chroma
+    storage_context = StorageContext.from_defaults(vector_store=chroma_vector_store)
+
     # ---- Index Creation ----
 
     # use api auto splitter chunk create index 
     # show_progress is a optional parameter, default is False
-    index = VectorStoreIndex.from_documents(documents=documents, show_progress=True) 
+    index = VectorStoreIndex.from_documents(documents=documents, show_progress=True, storage_context=storage_context) 
 
     # manual create index    
     # index = VectorStoreIndex.from_documents(documents=documents, transformations=[text_splitter])
@@ -68,6 +83,7 @@ async def main():
     # this step is the final step of rag (use user's query to get relevant info, location source files)
     response = query_engine.query("Test Engineer")
     print(response)
+
 if __name__ == "__main__":
     import asyncio
     asyncio.run(main())
